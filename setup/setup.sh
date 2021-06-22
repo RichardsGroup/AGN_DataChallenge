d5ee2b1559e6d53f3324f35abe483929920f1808
Weixiang Yu <wy73@drexel.edu>
2021-06-22 00:04:25 +0000

    simple instructions to get started

---
+++ setup/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# activate shell for conda
+echo ". /home/idies/miniconda3/etc/profile.d/conda.sh" >> ~/.bashrc
+source ~/.bashrc
+
+# create a new conda environment and activate 
+conda env create --file env_sciserver.yml
+conda activate agn_dc
+
+# install new ipython kernel
+python -m ipykernel install --user --name lsst_train --display-name "AGN DC"
+
+# source again to activate current shell and conda env
+source ~/.bashrc
